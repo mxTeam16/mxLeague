1d4d35aff7d7cf55f024b0bfde9ad5b4fbf88b4a
dtls <dtls@dtlscom>
2016-01-25 13:24:36 +0100

    Added Eureka registration server

--- boot/src/main/java/com/mxleague/boot/FirstService.java
+++ boot/src/main/java/com/mxleague/boot/FirstService.java
@@ -2,15 +2,19 @@ package com.mxleague.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class FirstService {
-	public static void main(String[] args) {
-		System.setProperty("server.port", "8187");
-		SpringApplication.run(FirstService.class, args);
-	}
+@EnableDiscoveryClient
+public class FirstService 
+{
+    public static void main( String[] args )
+    {
+     System.setProperty("spring.config.name", "first-service");
+      SpringApplication.run(FirstService.class, args);
+    }
 }
