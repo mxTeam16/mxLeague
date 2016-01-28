e41ebb7bcc7e728be5c00705accd9d05114d02b2
Victor M <vrmr@gft.com>
2016-01-28 12:42:44 +0100

    Add security with database (it needs to merge with master to get necessary classes)

--- boot/src/main/java/com/mxleague/boot/config/SecurityConfiguration.java
+++ boot/src/main/java/com/mxleague/boot/config/SecurityConfiguration.java
@@ -1,27 +1,56 @@
 package com.mxleague.boot.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new StandardPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity
+		  .formLogin()
+		     .permitAll()
+		     .and()
+	      .authorizeRequests()
+	         .antMatchers("/rest/**")
+	         .permitAll()
+	         .and()
+	      .authorizeRequests()
+	         .antMatchers("**")
+             .hasAuthority("R");
+	}
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 
-		web.ignoring().antMatchers("/api/*");
+		//web.ignoring().antMatchers("/api/*");
 
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		auth.userDetailsService(userDetailsService);// .passwordEncoder(passwordEncoder());
 	}
 
 }
