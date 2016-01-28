813d287f8a36d491de03af4cbfe475b055c34b0a
Miguel Gonzales <Miguel-Angel.Gonzales@gft.com>
2016-01-28 15:33:19 +0100

    REST - HATEOAS (role)

---
+++ boot/src/main/java/com/mxleague/boot/repo/RoleRepo.java
@@ -0,0 +1,9 @@
+package com.mxleague.boot.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mxleague.boot.domain.Role;
+
+public interface RoleRepo extends CrudRepository<Role, String> {
+	
+}
