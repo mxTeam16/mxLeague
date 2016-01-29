f901e82643a954f501400a7bc099f2e84f0e4eb8
dtls <dtls@PCMAD366.gft.com>
2016-01-29 09:10:24 +0100

    merged with security + fixed properties

--- client/src/main/java/com/mxleague/client/TestClient.java
+++ client/src/main/java/com/mxleague/client/TestClient.java
@@ -1,6 +1,5 @@
 package com.mxleague.client;
 
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
