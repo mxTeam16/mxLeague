a404a1dd4b843d649916c3a4f45604e689969fcb
Miguel Gonzales <Miguel-Angel.Gonzales@gft.com>
2016-02-03 09:28:07 +0100

    REST, HATEOAS (transfers)

--- boot/src/main/java/com/mxleague/boot/repo/PlayerRepo.java
+++ boot/src/main/java/com/mxleague/boot/repo/PlayerRepo.java
@@ -9,4 +9,7 @@ public interface PlayerRepo extends CrudRepository<Player, String> {
 
 	@Query("SELECT p FROM Player p WHERE LOWER(p.id_player) = LOWER(?1) AND LOWER(p.statistic) = LOWER(?2)")
 	Player findById_PlayerAndId_StatisticCaseInsensitive(String id_player, String id_statistic);
+	
+	@Query("SELECT p FROM Player p WHERE LOWER(p.id_player) = LOWER(?1) AND LOWER(p.transfer) = LOWER(?2)")
+	Player findById_PlayerAndId_TransferCaseInsensitive(String id_player, String id_trasnfer);
 }
--- boot/src/main/java/com/mxleague/boot/repo/TransferRepo.java
+++ boot/src/main/java/com/mxleague/boot/repo/TransferRepo.java
@@ -2,8 +2,8 @@ package com.mxleague.boot.repo;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.mxleague.boot.domain.Player;
+import com.mxleague.boot.domain.Transfer;
 
-public interface TransferRepo extends CrudRepository<Player, String> {
+public interface TransferRepo extends CrudRepository<Transfer, String> {
 
 }
